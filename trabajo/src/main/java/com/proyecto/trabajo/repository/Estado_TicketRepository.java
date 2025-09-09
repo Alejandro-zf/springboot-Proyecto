072c9fb7f7dd4d532a9ebf2f016f5e5fee82f760
jeison <jeisonfelipecas08@gmail.com>
2025-09-09 16:40:00 -0500

    repositories

---
+++ trabajo/src/main/java/com/proyecto/trabajo/repository/Estado_TicketRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.trabajo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.trabajo.models.Estado_ticket;
+
+public interface Estado_TicketRepository extends JpaRepository<Estado_ticket,Byte> {
+
+}
