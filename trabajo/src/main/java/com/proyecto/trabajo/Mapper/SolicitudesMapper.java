93ddfcf1a96d7edc527a146d4ac789e14817de81
adriX'O <adriandjpc@hotmail.com>
2025-09-16 15:11:54 -0500

    elimine los mapper en service

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/SolicitudesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.SolicitudesDto;
-import com.proyecto.trabajo.models.Solicitudes;
-
-public interface SolicitudesMapper {
-    Solicitudes toEntity(SolicitudesDto dto);
-    SolicitudesDto toDTO(Solicitudes entity);
-}
--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/TicketsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.TicketsDtos;
-import com.proyecto.trabajo.models.Tickets;
-
-public interface TicketsMapper {
-    Tickets toEntity(TicketsDtos dto);
-    TicketsDtos toDTO(Tickets entity);
-}
