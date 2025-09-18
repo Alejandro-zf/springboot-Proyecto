7e15cfe23b446027defd66eabc17fd801bb98584
juanda <cruzjuanda8a@gmail.com>
2025-09-18 16:50:51 -0500

    Terminacion de algunos controllers

--- trabajo/src/main/java/com/proyecto/trabajo/Controller/ElementoController.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Controller/ElementoController.java
@@ -8,9 +8,16 @@ import com.proyecto.trabajo.dto.ElementoDto;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -26,9 +33,31 @@ public class ElementoController {
     //Crear elemento
     @PostMapping
     public ResponseEntity<ElementoDto> crear (@Valid @RequestBody ElementoDto dto){
-        ElementoDto elemento = elementosServices.guardar(dto);
-        
+        ElementoDto creado = elementosServices.guardar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado);   
     }
-        
+
+    //Obtener por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ElementoDto> obtenerPorId(@PathVariable Long id){
+        ElementoDto elemento = elementosServices.buscarPorId(id);
+        return ResponseEntity.ok(elemento); 
     }
+
+    //Listar todos
+    @GetMapping
+    public ResponseEntity<List<ElementoDto>> listarTodos(){
+        List<ElementoDto> elementos = elementosServices.listarTodos();
+        return ResponseEntity.ok(elementos);
+    }
+
+    //Eliminar elementos
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar (@PathVariable Long id){
+        elementosServices.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
     
