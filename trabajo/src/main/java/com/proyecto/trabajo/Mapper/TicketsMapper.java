8e66dd00c982a62e0344af0b59e3d353e66dc538
daniel_parra16 <adriandjpc@hotmail.com>
2025-09-18 16:41:47 -0500

    subo cambios

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/TicketsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.TicketsDtos;
-import com.proyecto.trabajo.models.Tickets;
-
-public interface TicketsMapper {
-    Tickets toTickets(TicketsDtos ticketsDto);
-    TicketsDtos toTicketsDto(Tickets tickets);
-}
