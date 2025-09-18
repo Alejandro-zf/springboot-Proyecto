3d1e2a35634e2ddaf9124720f0fad7258ddf1fdd
anderson001-ops <joelcapera2023@gmail.com>
2025-09-18 01:06:38 -0500

    elimino

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/ElementosMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.ElementoDto;
-import com.proyecto.trabajo.models.Elementos;
-
-public interface ElementosMapper {
-    Elementos toEntity(ElementoDto dto);
-    ElementoDto toDTO(Elementos entity);
-}
--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/PrestamosMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.PrestamosDto;
-import com.proyecto.trabajo.models.Prestamos;
-
-public interface PrestamosMapper {
-    Prestamos toEntity(PrestamosDto dto);
-    PrestamosDto toDTO(Prestamos entity);
-}
