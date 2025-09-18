ab41759eefa2fd5dc682c0f8bb5f1d4960fac1a7
anderson <joelcapera2023@gmail.com>
2025-09-18 17:21:04 -0500

    actualizar Mapper

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/SolicitudesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.trabajo.Mapper;
-
-import com.proyecto.trabajo.dto.SolicitudesDto;
-import com.proyecto.trabajo.models.Solicitudes;
-
-public interface SolicitudesMapper {
-    Solicitudes toSolicitudes(SolicitudesDto solicitudesdto);
-    SolicitudesDto toSolicitudesDTO(Solicitudes solicitudes);
-}
