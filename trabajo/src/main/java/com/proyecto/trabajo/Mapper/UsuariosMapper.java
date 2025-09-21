4f20da53307d0bd5ee7f292cf70614ebccd228e6
adriXO06 <adriandjpc@hotmail.com>
2025-09-20 23:31:24 -0500

    mapper de y services de usuarios

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/UsuariosMapper.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Mapper/UsuariosMapper.java
@@ -1,9 +1,13 @@
 package com.proyecto.trabajo.Mapper;
 
 import com.proyecto.trabajo.dto.UsuariosDto;
+import com.proyecto.trabajo.dto.UsuariosCreateDto;
+import com.proyecto.trabajo.dto.UsuariosUpdateDto;
 import com.proyecto.trabajo.models.Usuarios;
 
 public interface UsuariosMapper {
     Usuarios toUsuarios(UsuariosDto usuariosDto);
     UsuariosDto toUsuariosDto(Usuarios usuarios);
+    Usuarios toUsuariosFromCreateDto(UsuariosCreateDto createDto);
+    Usuarios toUsuariosFromUpdateDto(UsuariosUpdateDto updateDto);
 }
