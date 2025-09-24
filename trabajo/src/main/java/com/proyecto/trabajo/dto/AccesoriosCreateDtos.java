5ee746b3f216c72e552b258a1bb6654d1f0621dc
jeison <jeisonfelipecas08@gmail.com>
2025-09-24 12:56:49 -0500

    crete dtos

---
+++ trabajo/src/main/java/com/proyecto/trabajo/dto/AccesoriosCreateDtos.java
@@ -0,0 +1,16 @@
+package com.proyecto.trabajo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AccesoriosCreateDtos {
+    private Integer id_accesorio;
+    private Integer canti;
+    private String nom_acces;
+    private String marc;
+    private Integer num_ser;
+}
---
+++ trabajo/src/main/java/com/proyecto/trabajo/dto/SolicitudeCreateDto.java
@@ -0,0 +1,18 @@
+package com.proyecto.trabajo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SolicitudeCreateDto {
+    private Long id_soli;
+    private Integer cant;
+    private LocalDateTime fecha_ini;
+    private LocalDateTime fecha_fn;
+    private String ambient;
+}
