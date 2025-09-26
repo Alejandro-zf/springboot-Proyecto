476b7cbd87d2d2d6d85c531ff4cf3d06457adb38
Juanda <cruzjuanda8a@gmail.com>
2025-09-26 15:18:03 -0500

    Creacion de models de las otras tablas de muchos a muchos

--- trabajo/src/main/java/com/proyecto/trabajo/models/Accesorios.java
+++ trabajo/src/main/java/com/proyecto/trabajo/models/Accesorios.java
@@ -1,10 +1,14 @@
 package com.proyecto.trabajo.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,4 +27,7 @@ public class Accesorios {
     @Column(nullable = false,length = 20)
     private String marca;
     private Integer num_serie;
+
+    @OneToMany(mappedBy = "accesorios", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Accesorios_solicitudes> accesolicitudess;
 }
