59160cdf2b173994d591882989d1551727b1064f
jeison <jeisonfelipecas08@gmail.com>
2025-10-03 15:02:56 -0500

    elementos

---
+++ trabajo/src/main/java/com/proyecto/trabajo/dto/ElementoUpdateDtos.java
@@ -0,0 +1,12 @@
+package com.proyecto.trabajo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ElementoUpdateDtos {
+    private Byte est_elem;
+}
