7ba150485ee91b1def0360fb0355ed0591028546
Juanda <cruzjuanda8a@gmail.com>
2025-10-02 19:32:36 -0500

    cambios en controller usuarios, agregacion de actualizar ususarios

--- trabajo/src/main/java/com/proyecto/trabajo/Controller/PrestamosController.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Controller/PrestamosController.java
@@ -9,9 +9,7 @@ import com.proyecto.trabajo.dto.PrestamosDto;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import java.util.List;
 import java.util.Map;
