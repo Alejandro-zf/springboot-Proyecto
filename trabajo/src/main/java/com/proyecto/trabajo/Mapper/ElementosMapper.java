4a49a9607973de42ef658f7ae3d39591a993ef91
adriXO06 <adriandjpc@hotmail.com>
2025-10-04 00:00:47 -0500

    cambios en elemento

--- trabajo/src/main/java/com/proyecto/trabajo/Mapper/ElementosMapper.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Mapper/ElementosMapper.java
@@ -2,10 +2,12 @@ package com.proyecto.trabajo.Mapper;
 
 import com.proyecto.trabajo.dto.ElementoDto;
 import com.proyecto.trabajo.dto.ElementosCreateDto;
+import com.proyecto.trabajo.dto.ElementoUpdateDtos;
 import com.proyecto.trabajo.models.Elementos;
 
 public interface ElementosMapper {
     Elementos toElementos(ElementoDto elementoDto);
     ElementoDto toElementoDto(Elementos elementos);
     Elementos toElementosFromCreateDto(ElementosCreateDto createDto);
+    void updateElementosFromUpdateDto(ElementoUpdateDtos updateDto, Elementos entity);
 }
