0c01332a52c09ad804466321ffe50bbb388674f7
Alejandro <johanalejandrobernal@gmail.com>
2025-10-04 12:08:23 -0500

    Relaciones con elementos y tickets

--- trabajo/src/main/java/com/proyecto/trabajo/models/Elementos.java
+++ trabajo/src/main/java/com/proyecto/trabajo/models/Elementos.java
@@ -48,4 +48,6 @@ public class Elementos {
     @OneToMany(mappedBy = "elementos")
     private List <Prestamos_Elemento> prestamosselemen;
 
+    @OneToMany(mappedBy = "tickets")
+    private List <Tickets> ticketss;
 }
