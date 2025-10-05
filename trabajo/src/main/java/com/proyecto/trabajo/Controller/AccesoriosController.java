6194c7bd48a712400139885e56b0907475530caa
anderson001-ops <joelcapera2023@gmail.com>
2025-10-05 02:02:57 -0500

    getmapping en controller

--- trabajo/src/main/java/com/proyecto/trabajo/Controller/AccesoriosController.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Controller/AccesoriosController.java
@@ -1,5 +1,15 @@
 package com.proyecto.trabajo.Controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,19 +18,6 @@ import com.proyecto.trabajo.dto.AccesoriosCreateDtos;
 import com.proyecto.trabajo.dto.AccesoriosDto;
 
 import jakarta.validation.Valid;
-
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -50,7 +47,13 @@ public class AccesoriosController {
                 .body(Map.of("error","Error al crear el accesorio","detalle", ex.getMessage()));
         }
     }
-     
+    
+    @GetMapping
+    public ResponseEntity<List<AccesoriosDto>> listarTodos() {
+        List<AccesoriosDto> accesorios = accesoriosServices.listarTodos();
+        return ResponseEntity.ok(accesorios);
+    }
+    
     //obtener por id 
     @GetMapping("/{id}")
     public ResponseEntity<AccesoriosDto> buscarPorId (@PathVariable Integer id) {
