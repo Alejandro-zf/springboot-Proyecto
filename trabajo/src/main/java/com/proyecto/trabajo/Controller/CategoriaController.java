bf8adfcec5c44a42c1418dc8a9f4acb1171416e2
LaSintaxis <nataliamariacardenasgarcia@gmail.com>
2025-10-20 16:52:22 -0500

    agregacion de rutas de controller

--- trabajo/src/main/java/com/proyecto/trabajo/Controller/CategoriaController.java
+++ trabajo/src/main/java/com/proyecto/trabajo/Controller/CategoriaController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.trabajo.Services.CategoriaServices;
 import com.proyecto.trabajo.dto.CategoriaCreateDtos;
@@ -17,6 +19,9 @@ import com.proyecto.trabajo.dto.CategoriaDtos;
 
 import jakarta.validation.Valid;
 
+
+@RestController
+@RequestMapping("/api/categoria")
 public class CategoriaController {
 
     private final CategoriaServices categoriaServices;
@@ -25,7 +30,7 @@ public class CategoriaController {
         this.categoriaServices = categoriaServices;
     }
 
-    //crear Categoria 
+    //crear Categoria
     @PostMapping
     public ResponseEntity<?> crear (@Valid @RequestBody CategoriaCreateDtos dto){
         try{
@@ -34,7 +39,7 @@ public class CategoriaController {
             .body(Map.of("mensaje","Categoria creada exitosamente","data",creado));
         }catch (IllegalStateException ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(Map.of("errores1", ex.getMessage()));    
+                .body(Map.of("errores1", ex.getMessage()));
         }
     }
 
